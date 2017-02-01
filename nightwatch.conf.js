331a8d1758b09bc7075b796c951ae047ab8ff986
felipehsoliveira <felipe.oliveira@concrete.com.br>
2017-02-01 13:37:15 -0200

    cucumberjs

---
+++ nightwatch.conf.js
@@ -0,0 +1,38 @@
+require('babel-register')
+require('nightwatch-cucumber')({
+  cucumberArgs: ['--require', 'timeout.js', '--require', 'features/step_definitions', '--format', 'pretty', '--format', 'json:reports/cucumber.json', 'features']
+})
+
+// http://nightwatchjs.org/guide#settings-file
+module.exports = {
+	src_folders: [],
+	output_folder: '',
+	selenium: {
+		start_process: true,
+		server_path: 'node_modules/selenium-server/lib/runner/selenium-server-standalone-3.0.1.jar',
+		host: '127.0.0.1',
+		port: 4444,
+    cli_args: {
+		  'webdriver.gecko.driver': require('geckodriver').path,
+      'webdriver.chrome.driver': require('chromedriver').path
+		}
+	},
+
+	test_settings: {
+		default: {
+			launch_url: 'http://localhost',
+			selenium_port: 4444,
+			selenium_host: 'localhost',
+			silent: true,
+			screenshots: {
+				enabled: false,
+				path: ''
+			},
+			desiredCapabilities: {
+				browserName: 'chrome',
+				javascriptEnabled: true,
+				acceptSslCerts: true
+			}
+		}
+	}
+}
